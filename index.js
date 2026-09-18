4819026b4bdd8bbb567982eedb01d61d4658a8ec
mateus <mateusweslley14@gmail.com>
2026-09-18 14:14:25 -0300

    Refactor handleRequest to improve flight selection logic and enhance user experience

--- index.js
+++ index.js
@@ -32,35 +32,30 @@ exports.handler = async (event, context) => {
 
 /* PROCESS INBOUND MESSAGE */
 function handleRequest(request) {
-  const input = (request.inputTranscript || "").trim();
-  request.currentIntent = request.interpretations[0].intent;
-  const current_intent = request.currentIntent.name;
-  const current_state = request.sessionState?.intent?.state || null;
-
-  console.log()
+  const input = (request.inputTranscript || "").trim(); // Texto de entrada do usuário
+  request.currentIntent = request.interpretations[0].intent; // Atualiza a intenção atual com base na interpretação mais recente
+  const current_intent = request.currentIntent.name; // Nome da intenção atual
+  const current_state = request.sessionState?.intent?.state || null; // Estado da intenção atual
 
   const slots =
     request.sessionState?.intent?.slots ||
     request.currentIntent.slots ||
-    {};
+    {}; // Slots da intenção atual
 
   const interactiveValue =
     slots.interactiveOption?.value?.interpretedValue ||
     slots.interactiveOption?.value?.originalValue ||
-    null;
+    null; // Valor do slot interactiveOption, se presente
 
-  const chosenAction = interactiveValue || input;
+  const chosenAction = interactiveValue || input; // Ação escolhida pelo usuário, seja do slot ou do input
 
   // Ações do menu (Book a Flight, etc.)
   if (Object.values(ACTIONS).includes(chosenAction)) {
     console.log("Ação do menu detectada, chamando handleActionResponse");
     return handleActionResponse(input, request);
   }
-  if (
-    request.invocationSource === "FulfillmentCodeHook" ||
-    (current_intent === "BookFlight" && current_state === "ReadyForFulfillment")
-  ) {
-    console.log("BookFlight ReadyForFulfillment → pedindo confirmação");
+  // ReadyForFulfillment: Todos slots preenchidos, pronto para executar lógica de negócio 
+  if (current_intent === "BookFlight" && current_state === "ReadyForFulfillment") {
 
     const sessionAttributes = request.sessionState?.sessionAttributes || {};
     const intentSlots = request.sessionState?.intent?.slots || {};
@@ -114,20 +109,100 @@ function handleRequest(request) {
     };
   }
 
-  if (current_intent === "BookFlight" && confirm) {
-    // exemplo: depois que confirmChoice veio preenchido
-    const confirm =
-      slots.confirmChoice?.value?.interpretedValue || input;
+  if (current_intent === "SelectFlight") {
 
-    const normalized = String(confirm).toLowerCase();
-    const ok = ["yes", "yeah", "yep", "confirm"].includes(normalized);
+    const fSelectValue = request.sessionState?.intent?.slots?.FSelect?.value?.interpretedValue;
 
-    return formTerminalResponse(
-      request.sessionState?.sessionAttributes || {},
-      ok ? "Fulfilled" : "Failed",
-      "BookFlight",
-      ok ? "Booking confirmed." : "Booking cancelled."
-    );
+    const sessionAttributes = request.sessionState?.sessionAttributes || {};
+    const intentSlots = request.sessionState?.intent?.slots || {};
+
+    if (fSelectValue) {
+      return {
+        sessionState: {
+          sessionAttributes,
+          dialogAction: {
+            type: "Close"
+          },
+          intent: {
+            name: "SelectFlight",
+            state: "Fulfilled",
+            slots: intentSlots
+          }
+        },
+        messages: [
+          {
+            contentType: "PlainText",
+            content: `Opção de vôo ${fSelectValue} selecionada com sucesso!`
+          }
+        ]
+      }
+    }
+    const title = "Please select your flight option:";
+
+    const template = {
+      templateType: "ListPicker",
+      version: "1.0",
+      data: {
+
+        content: {
+          title: title,
+          subtitle: "Tap to select option",
+          imageType: "URL",
+          imageData: "https://readthemanual3.s3.us-east-1.amazonaws.com/azul.jpg",
+          imageDescription: "Select any of the option",
+          elements: [
+            {
+              title: "Select 1",
+              subtitle: "13:40 - 21:40, ✈️ 6E919, $65",
+              imageType: "URL",
+              imageData: "https://readthemanual3.s3.us-east-1.amazonaws.com/aviao.jpg"
+            },
+            {
+              title: "Select 2",
+              subtitle: "06:10 - 11:40, ✈️ 6E920, $45",
+              imageType: "URL",
+              imageData: "https://readthemanual3.s3.us-east-1.amazonaws.com/aviao.jpg",
+            },
+            {
+              title: "Select 3",
+              subtitle: "09:00 - 15:00, ✈️ 6E921, $55",
+              imageType: "URL",
+              imageData: "https://readthemanual3.s3.us-east-1.amazonaws.com/aviao.jpg",
+            },
+            {
+              title: "Select 4",
+              subtitle: "15:00 - 21:00, ✈️ 6E922, $75",
+              imageType: "URL",
+              imageData: "https://readthemanual3.s3.us-east-1.amazonaws.com/aviao.jpg",
+            }
+          ]
+        }
+      }
+    }
+
+    const slotToElicit = "FSelect";
+
+    return {
+      sessionState: {
+        sessionAttributes,
+        dialogAction: {
+          type: "ElicitSlot",
+          slotToElicit,
+        },
+        intent: {
+          name: "SelectFlight",
+          state: "InProgress",
+          slots: intentSlots,
+          confirmationState: "None",
+        },
+      },
+      messages: [
+        {
+          contentType: "CustomPayload",
+          content: JSON.stringify(template),
+        },
+      ],
+    };
   }
 
   // Se o input for um pedido de ajuda ou o início da intenção Help
